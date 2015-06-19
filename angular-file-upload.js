0f7210b2602d8345fc75353653a48941392083fa
ThomGonzalez <thom.sgonzalez@gmail.com>
2015-06-19 18:22:01 -0500

    Custom X-CSRFToken header to the add value CSRF token POST request Django.

--- angular-file-upload.js
+++ angular-file-upload.js
@@ -497,9 +497,28 @@ module
                     that._onCompleteItem(item, response, xhr.status, headers);
                 };
 
+                // using jQuery
+                function getCookie(name) {
+                    var cookieValue = null;
+                    if(document.cookie && document.cookie != ''){
+                        var cookies = document.cookie.split(';');
+                        for (var i = 0; i < cookies.length; i++){
+                            var cookie = jQuery.trim(cookies[i]);
+                            // Does this cookie string begin with the name we want?
+                            if(cookie.substring(0, name.length + 1) == (name + '=')){
+                                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                                break;
+                            }
+                        }
+                    }
+                    return cookieValue;
+                };
+                
                 xhr.open(item.method, item.url, true);
 
                 xhr.withCredentials = item.withCredentials;
+                // Add csrftoken
+                xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
 
                 angular.forEach(item.headers, function(value, name) {
                     xhr.setRequestHeader(name, value);
